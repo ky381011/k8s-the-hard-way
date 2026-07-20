cf7519ea48dc9b331eb5dc4b1b9144e71c385d62
ky381011 <ky381011git@gmail.com>
2026-07-20 15:30:51 +0900

    Terraformプロバイダー設定ファイルを追加

---
+++ src/hosts/layer/bootstrap/terraform/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = "~> 2.9"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token    = var.proxmox_api_token
+  pm_tls_insecure = var.proxmox_tls_insecure
+}
