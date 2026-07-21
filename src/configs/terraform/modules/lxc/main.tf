4047ae780e77436f6f07e5435787fba017c8a376
ky381011 <ky381011git@gmail.com>
2026-07-21 22:59:38 +0900

    LXCモジュールのmain.tfとvariables.tfを追加し、ターゲットノードの設定を定義

---
+++ src/configs/terraform/modules/lxc/main.tf
@@ -0,0 +1,3 @@
+resource "proxmox_lxc" "this" {
+  target_node = var.target_node
+}
---
+++ src/configs/terraform/modules/lxc/variables.tf
@@ -0,0 +1,5 @@
+variable "target_node" {
+  description = "A string containing the cluster node name"
+  type        = string
+  default     = null
+}
