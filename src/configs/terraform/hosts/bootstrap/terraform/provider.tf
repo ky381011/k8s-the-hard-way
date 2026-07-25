76ac4b1f8ec242bb9e327fa49575b3f52cf40078
ky381011 <ky381011git@gmail.com>
2026-07-25 23:35:24 +0900

    プロバイダー設定を更新し、Proxmoxのソースとバージョンを変更。APIエンドポイントとトークン認証を追加。

--- src/configs/terraform/hosts/bootstrap/terraform/provider.tf
+++ src/configs/terraform/hosts/bootstrap/terraform/provider.tf
@@ -1,15 +1,18 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = "~> 2.9"
+      source  = "bpg/proxmox"
+      version = "0.111.1"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url      = var.provider_configs.pm_api_url
-  pm_api_token_id = var.provider_configs.pm_api_token_id
-  pm_api_token    = var.provider_configs.pm_api_token
-  pm_tls_insecure = var.provider_configs.pm_tls_insecure
+  endpoint = var.proxmox_endpoint
+
+  # APIトークン認証（推奨）
+  api_token = var.proxmox_api_token
+
+  # 自己署名証明書の場合
+  insecure = true
 }
